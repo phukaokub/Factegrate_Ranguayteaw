53065860a75ab84e3a9d139a3d24d00072d1917b
Krittin Thirasak <krittin.thirasak@gmail.com>
2022-08-31 16:00:54 +0700

    Add Database & TSS file
    By Krittin

---
+++ generateDB.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from urllib import response
+import requests
+import json
+from csv import writer
+
+def appendRow(row): # Append data as a new row
+    with open('database.csv', 'a+', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+        csv_writer.writerow(row)
+
+def getData(): # Get color from sensor 10
+    responseAPI = requests.get('http://localhost/tss/0/sensor/10')
+
+    data = responseAPI.text
+    parseJson = json.loads(data)
+
+    # Get color
+    color =  parseJson['value']
+    print(f'Color : {color}')
+
+    # Get date & time
+    now = datetime.now()
+    year = now.date().year
+    month = now.date().month
+    day = now.date().day
+    print(f'Date : {year} {month} {day}')
+    hour = now.time().hour
+    min = now.time().minute
+    sec = now.time().second
+    print(f'Time : {hour} {min} {sec}') 
+
+    # Get factory section
+    section = 'Noodle Sorting'
+
+    # Update new data to the next row
+    newData = [year, month, day, hour, min, sec, section, color]
+    appendRow(newData)
+
